be7b020d7be768ebaa73c1290433351ec3d8acb7
root <me@morxw.com>
2020-02-05 23:16:49 +0100

    Initial commit

---
+++ config.js
@@ -0,0 +1,60 @@
+module.exports = {
+  listenIp: '192.168.56.101',
+  listenPort: 3001,
+  sslCrt: './certs/server.cert',
+  sslKey: './certs/server.key',
+  mediasoup: {
+    // Worker settings
+    worker: {
+      rtcMinPort: 40000,
+      rtcMaxPort: 40100,
+      logLevel: 'debug',
+      logTags: [
+        'info',
+        'ice',
+        'dtls',
+        'rtp',
+        'srtp',
+        'rtcp',
+        'rtx',
+        'bwe',
+        'score',
+        'simulcast',
+        'svc',
+        'debug'
+      ],
+    },
+    // Router settings
+    router: {
+      mediaCodecs:
+        [
+          {
+            kind: 'audio',
+            mimeType: 'audio/opus',
+            clockRate: 48000,
+            channels: 2
+          },
+          {
+            kind: 'video',
+            mimeType: 'video/VP8',
+            clockRate: 90000,
+            parameters:
+              {
+                'x-google-start-bitrate': 1000
+              }
+          },
+        ]
+    },
+    // WebRtcTransport settings
+    webRtcTransport: {
+      listenIps: [
+        {
+          ip: '192.168.56.101',
+          announcedIp: null,
+        }
+      ],
+      maxIncomingBitrate: 1500000,
+      initialAvailableOutgoingBitrate: 1000000,
+    }
+  }
+};
